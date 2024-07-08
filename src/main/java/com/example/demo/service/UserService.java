09b7bf71487ae015a3076cb93924e89c89b6a13b
조용진 <101401582+Brio-yj@users.noreply.github.com>
2024-07-08 14:14:33 +0900

    feat: Added user registration API
    Only implemented registration functionality for now. Planning to
    segregate user operations based on roles and include CRUD operations in
    the future.
    I#	modified:   build.gradle

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.User;
+import com.example.demo.dto.UserDto;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    public User reister(UserDto userDto){
+        User user = new User();
+        user.setUsername(userDto.getUsername());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setRole(userDto.getRole());
+        return userRepository.save(user);
+    }
+}
