8d2daaf03cfe4bb048c300d7458e9ccb9902a128
조용진 <101401582+Brio-yj@users.noreply.github.com>
2024-07-10 10:50:38 +0900

    change directory structure

--- src/main/java/com/example/demo/util/Const.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.util;
-
-public class Const {
-    public static final String LOGIN_SUCCESS_CODE = "LOGIN_SUCCESS";
-    public static final String LOGIN_FAILURE_CODE = "LOGIN_FAILURE";
-}
