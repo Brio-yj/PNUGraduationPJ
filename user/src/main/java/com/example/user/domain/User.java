fe5a1e2f90e7b529ecbec2708457cc3cf1ca5502
조용진 <101401582+Brio-yj@users.noreply.github.com>
2024-07-10 14:04:23 +0900

    fetch : removed the external access blocking feature of Spring Security. Create a project using Spring Boot to access the external IP of a Kubernetes cluster using RestTemplate, fetching information about the currently running pods.

---
+++ user/src/main/java/com/example/user/domain/User.java
@@ -0,0 +1,17 @@
+package com.example.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
+}
---
+++ user/src/main/java/com/example/user/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.example.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String username;
+    private String password;
+    private String role;
+}
